30ebf48c1983d2cf9bfd462c8a6c26255b020537
Josh Hinchliffe <joshuahinchliffe97@gmail.com>
2023-04-21 14:44:40 +0100

    Added code to hopefully read ORP and pH. Code is untested as can't use sensors currently.

--- ArduinoWriter/arduinoWriter/arduinoWriter.ino
+++ ArduinoWriter/arduinoWriter/arduinoWriter.ino
@@ -1,5 +1,6 @@
 #include <ArduinoJson.h>
 #include "DFRobot_ORP_PRO.h"
+#include "DFRobot_PH.h"
 
 // Water Pressure Sensor Wiki
 // https://wiki.dfrobot.com/Gravity__Water_Pressure_Sensor_SKU__SEN0257
@@ -10,16 +11,13 @@
 // Water Flow Rate Sensor Docs
 // https://www.hobbytronics.co.uk/datasheets/sensors/YF-S201.pdf
 
-// ORP Sensor Docs
+// Water pH Sensor Docs
+// https://wiki.dfrobot.com/Gravity__Analog_pH_Sensor_Meter_Kit_V2_SKU_SEN0161-V2
+
+// Water ORP Sensor Docs
 // https://wiki.dfrobot.com/Gravity_Analog_ORP_Sensor_PRO_SKU_SEN0464
 
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  while(!Serial) continue;
-  Serial.println("Initialising");
-}
 
 int count = 0;
 
@@ -27,16 +25,30 @@ int PRES_PIN_1 = 6;
 int PRES_PIN_2 = 7;
 int TURB_PIN = 9;
 int ORP_PIN =  11;
+int PH_PIN = 13;
 float PRES_CAL_1 = 5.00;
 float PRES_CAL_2 = 5.00;
 float PRES_1 = 0;
 float PRES_2 = 0;
 float TURB = 0;
-float ORP_VAL = 0;
+float ORP = 0;
+float PH = 0;
+float WATER_TEMP = 25;
 
 DynamicJsonDocument JSON_DOC(1024);
 
-DFRobot_ORP_PRO ORP(0);
+DFRobot_ORP_PRO ORPCalc(0);
+
+DFRobot_PH PhCalc;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  PhCalc.begin();
+  while(!Serial) continue;
+  Serial.println("Initialising");
+}
 
 float getWaterPressure(int port, double offset) {
   float v = analogRead(port) * 5.00 / 1024;     //Sensor output voltage
@@ -57,6 +69,18 @@ float getTurbidityVoltage(int port) {
   return analogRead(port) * (5.0 / 1024.0);
 }
 
+float getOrp(int port) {
+  float voltage = ((unsigned long)analogRead(ORP_PIN) * 5000 + 1024 / 2) / 1024;
+  float orp = ORPCalc.getORP(voltage);
+  return orp;
+}
+
+float getPh(int port) {
+  float voltage = analogRead(port)/1024.0*5000;
+  float phValue = PhCalc.readPH(voltage, WATER_TEMP);
+  return phValue;
+}
+
 void calibrateSensors() {
   PRES_CAL_1 = getWaterPressureCal(PRES_PIN_1);
   PRES_CAL_2 = getWaterPressureCal(PRES_PIN_2);
@@ -73,12 +97,16 @@ void updateMetrics() {
   JSON_DOC["PRES_CAL_2"] = PRES_CAL_2;
   JSON_DOC["PRES_2"] = PRES_2;
   JSON_DOC["TURB"] = TURB;
+  JSON_DOC["ORP"] = ORP;
+  JSON_DOC["PH"] = PH;
 }
 
 void readSensors() {
   PRES_1 = getWaterPressure(PRES_PIN_1, PRES_CAL_1);
   PRES_2 = getWaterPressure(PRES_PIN_2, PRES_CAL_2);
   TURB = getTurbidityVoltage(TURB_PIN);
+  ORP = getOrp(ORP_PIN);
+  PH = getPh(PH_PIN);
 }
 
 void loop() {
