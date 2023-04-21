6012249b736dc18a3c3af64bd8a0cf9b4ca6e479
Josh Hinchliffe <joshuahinchliffe97@gmail.com>
2023-04-21 11:57:12 +0100

    Added Turbidity reading and transmission

--- ArduinoWriter/arduinoWriter/arduinoWriter.ino
+++ ArduinoWriter/arduinoWriter/arduinoWriter.ino
@@ -1,5 +1,11 @@
 #include <ArduinoJson.h>
 
+// Water Pressure Sensor Wiki
+// https://wiki.dfrobot.com/Gravity__Water_Pressure_Sensor_SKU__SEN0257
+
+// Water Turbidity Sensor Wiki
+// https://wiki.dfrobot.com/Turbidity_sensor_SKU__SEN0189
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -10,11 +16,13 @@ void setup() {
 int count = 0;
 
 int PRES_PIN_1 = 6;
-float PRES_CAL_1 = 5.00;
-float PRES_1 = 0;
 int PRES_PIN_2 = 7;
+int TURB_PIN = 9;
+float PRES_CAL_1 = 5.00;
 float PRES_CAL_2 = 5.00;
+float PRES_1 = 0;
 float PRES_2 = 0;
+float TURB = 0;
 
 DynamicJsonDocument JSON_DOC(1024);
 
@@ -33,6 +41,10 @@ float getWaterPressureCal(int port) {
   return lowest * 5.00 / 1024;
 }
 
+float getTurbidityVoltage(int port) {
+  return analogRead(port) * (5.0 / 1024.0);
+}
+
 void calibrateSensors() {
   PRES_CAL_1 = getWaterPressureCal(PRES_PIN_1);
   PRES_CAL_2 = getWaterPressureCal(PRES_PIN_2);
@@ -48,11 +60,13 @@ void updateMetrics() {
   JSON_DOC["PRES_1"] = PRES_1;
   JSON_DOC["PRES_CAL_2"] = PRES_CAL_2;
   JSON_DOC["PRES_2"] = PRES_2;
+  JSON_DOC["TURB"] = TURB;
 }
 
 void readSensors() {
   PRES_1 = getWaterPressure(PRES_PIN_1, PRES_CAL_1);
   PRES_2 = getWaterPressure(PRES_PIN_2, PRES_CAL_2);
+  TURB = getTurbidityVoltage(TURB_PIN);
 }
 
 void loop() {
